9903233f6db793ff14c8f8321e3a60975892b141
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-11-21 11:35:41 -0500

    update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
     skip_before_action :authenticate_user
-    skip_before_action :verify_authenticity_token
     
     def index
         messages = Message.all
